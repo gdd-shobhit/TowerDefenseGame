adc57ef23188c82a6b333f1a54105b1d513a8563
sd5936 <sd5936@rit.edu>
2021-03-02 20:59:50 -0500

    Initialized

---
+++ TowerDefendGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Simple singelton
+    public static GameManager instance;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+}
