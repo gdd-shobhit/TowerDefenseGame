11a8ad2c141741888494e751793f0f7535ddf030
Marc Molnar <mgm1349@rit.edu>
2021-03-14 16:34:28 -0400

    Added path tiles and created a path layout.
    Tiles hold whether it is a path, whether it has a tower on top or not, and the position.
    The path manager creates the layout of the path and puts cubes onto the screen. These cubes are green if they are the path or brown if they are just normal tiles.

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile
+{
+    private bool pathTile = false;
+    private bool taken = false;
+    private Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // constructor accepts if its a path tile, if has a tower on top
+    //and a position on the screen
+    public Tile(bool vPathTile, bool vTaken, Vector3 vPosition)
+    {
+        pathTile = vPathTile;
+        taken = vTaken;
+        position = vPosition;
+    }
+
+    //grabs if this is a path tile
+    public bool getPathTile()
+    {
+        return pathTile;
+    }
+    
+    //returns if there is a tower on top
+    public bool getTaken()
+    {
+        return taken;
+    }
+
+    //can set if there is a tower on top
+    public void setTaken(bool vTaken)
+    {
+        taken = vTaken;
+    }
+
+    //grabs the position on the screen.
+    public Vector3 getPosition()
+    {
+        return position;
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    
+    public GameObject tilePrefab;
+    public Material pathTileMaterial;
+    public Material nonPathTileMaterial;
+    public Tile[,] tiles;
+
+
+    //level 1 for right now. Stars represent the path
+    //and the - represent open sapces
+    private char[,] level1 = { 
+        {'*','-','-','-','-','*','*','*','-','-'},
+        {'*','-','-','-','-','*','-','*','-','-'},
+        {'*','-','-','-','-','*','-','*','-','-'},
+        {'*','-','-','-','-','*','-','*','-','-'},
+        {'*','*','*','*','*','*','-','*','-','-'},
+        {'-','-','-','-','-','-','-','*','-','-'},
+        {'-','*','*','*','*','*','*','*','-','-'},
+        {'-','*','-','-','-','-','-','-','-','-'},
+        {'-','*','-','-','-','-','-','-','-','-'},
+        {'-','*','-','-','-','-','-','-','-','-'},};
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        tiles = new Tile[10,10];
+
+        //nested for loop to go through level1 layout
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+
+                //checks to see if the current tile 
+                //is a path or not
+                if (level1[i,j] == '*')
+                {
+                    //if so lable the tile as such and create a green tile to represent the path
+                    //note i and j had to be switched so the array would draw correctly to the screen (just changes position nothing in the array)
+                    tiles[i,j] = new Tile(true, false, new Vector3(j * 5, i * -5, 0));
+                
+                    //create the cube with a green color
+                    tilePrefab.GetComponent<MeshRenderer>().material = pathTileMaterial;
+                    Instantiate(tilePrefab, tiles[i, j].getPosition(), new Quaternion());
+                }
+                else
+                {
+                    //else do not lable the tile a path.
+                    //note the same thing as above.
+                    tiles[i, j] = new Tile(false, false, new Vector3(j * 5, i * -5, 0));
+                
+                    //create the cube with a brown color
+                    tilePrefab.GetComponent<MeshRenderer>().material = nonPathTileMaterial;
+                    Instantiate(tilePrefab, tiles[i, j].getPosition(), new Quaternion());
+                }
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
