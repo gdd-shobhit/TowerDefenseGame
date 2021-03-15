66fb0350902ece9e346d8a233e996d7889d49ae7
Marc Molnar <mgm1349@rit.edu>
2021-03-15 18:09:45 -0400

    Updated some nitpicky stuff

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Tile is a class that holds whether it is a path tile, if there is a 
+/// tower on it and the position,
+/// </summary>
 public class Tile
 {
     private bool pathTile = false;
     private bool taken = false;
     private Vector3 position = new Vector3(0.0f, 0.0f, 0.0f);
 
-    // constructor accepts if its a path tile, if has a tower on top
-    //and a position on the screen
+    /// <summary>
+    /// constructor accepts if its a path tile, if has a tower on top
+    /// and a position on the screen
+    /// </summary>
+    /// <param name="vPathTile"></param>
+    /// <param name="vTaken"></param>
+    /// <param name="vPosition"></param>
     public Tile(bool vPathTile, bool vTaken, Vector3 vPosition)
     {
         pathTile = vPathTile;
@@ -17,26 +26,38 @@ public class Tile
         position = vPosition;
     }
 
-    //grabs if this is a path tile
-    public bool getPathTile()
+    /// <summary>
+    /// grabs if this is a path tile
+    /// </summary>
+    /// <returns></returns>
+    public bool GetPathTile()
     {
         return pathTile;
     }
-    
-    //returns if there is a tower on top
-    public bool getTaken()
+
+    /// <summary>
+    /// returns if there is a tower on top
+    /// </summary>
+    /// <returns></returns>
+    public bool GetTaken()
     {
         return taken;
     }
 
-    //can set if there is a tower on top
-    public void setTaken(bool vTaken)
+    /// <summary>
+    /// can set if there is a tower on top
+    /// </summary>
+    /// <param name="vTaken"></param>
+    public void SetTaken(bool vTaken)
     {
         taken = vTaken;
     }
 
-    //grabs the position on the screen.
-    public Vector3 getPosition()
+    /// <summary>
+    /// grabs the position on the screen.
+    /// </summary>
+    /// <returns></returns>
+    public Vector3 GetPosition()
     {
         return position;
     }
--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -9,6 +9,8 @@ public class TileManager : MonoBehaviour
     public Material pathTileMaterial;
     public Material nonPathTileMaterial;
     public Tile[,] tiles;
+    public int mapHeight;
+    public int mapWidth;
 
 
     //level 1 for right now. Stars represent the path
@@ -25,47 +27,53 @@ public class TileManager : MonoBehaviour
         {'-','*','-','-','-','-','-','-','-','-'},
         {'-','*','-','-','-','-','-','-','-','-'},};
 
-
-    // Start is called before the first frame update
+    /// <summary>
+    /// Start initializes the path
+    /// </summary>
     void Start()
     {
-
-        tiles = new Tile[10,10];
+        mapHeight = 10;
+        mapWidth = 10;
+        tiles = new Tile[mapWidth, mapHeight];
 
         //nested for loop to go through level1 layout
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < mapWidth; i++)
         {
-            for (int j = 0; j < 10; j++)
+            for (int j = 0; j < mapHeight; j++)
             {
-
+                
                 //checks to see if the current tile 
                 //is a path or not
                 if (level1[i,j] == '*')
                 {
-                    //if so lable the tile as such and create a green tile to represent the path
+                    //if so label the tile as such and create a green tile to represent the path
                     //note i and j had to be switched so the array would draw correctly to the screen (just changes position nothing in the array)
                     tiles[i,j] = new Tile(true, false, new Vector3(j * 5, i * -5, 0));
                 
                     //create the cube with a green color
                     tilePrefab.GetComponent<MeshRenderer>().material = pathTileMaterial;
-                    Instantiate(tilePrefab, tiles[i, j].getPosition(), new Quaternion());
+                    Instantiate(tilePrefab, tiles[i, j].GetPosition(), new Quaternion());
                 }
                 else
                 {
-                    //else do not lable the tile a path.
+                    //else do not label the tile a path.
                     //note the same thing as above.
                     tiles[i, j] = new Tile(false, false, new Vector3(j * 5, i * -5, 0));
                 
                     //create the cube with a brown color
                     tilePrefab.GetComponent<MeshRenderer>().material = nonPathTileMaterial;
-                    Instantiate(tilePrefab, tiles[i, j].getPosition(), new Quaternion());
+                    Instantiate(tilePrefab, tiles[i, j].GetPosition(), new Quaternion());
                 }
             }
         }
 
     }
 
-    // Update is called once per frame
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
     void Update()
     {
     }
